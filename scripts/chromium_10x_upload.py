15a2e1475b1caf02e628a47494ab10824f5835f8
Sunny Sun <38218185+sunnyosun@users.noreply.github.com>
2024-12-02 22:12:43 +0100

    ♻️ Refactor (#6)
    * Refactor notebooks
    
    * 🎨 Refactor notebooks

--- scripts/chromium_10x_upload.py
+++ scripts/chromium_10x_upload.py
@@ -1,17 +1,31 @@
+import bionty as bt
 import lamindb as ln
 
 # track the current python script
-ln.settings.transform.stem_uid = "J5ZTmVxSch3U"
-ln.settings.transform.version = "1"
 ln.settings.sync_git_repo = "https://github.com/laminlabs/rnd-demo"
-run = ln.track(params={"project": "schmidt22"})
+ln.track("J5ZTmVxSch3U5zKv", params={"project": "schmidt22"})
 # label the transform
-ulabel1 = ln.ULabel(name="use-case").save()
-ulabel2 = ln.ULabel(name="schmidt22").save()
-run.transform.ulabels.add(ulabel1, ulabel2)
+ulabel1 = ln.ULabel.get(name="use-case")
+ulabel2 = ln.ULabel.get(name="schmidt22")
+ln.context.transform.ulabels.add(ulabel1, ulabel2)
+
+# label with instrument
+features = ln.Feature.lookup()
+novaseq6000 = bt.ExperimentalFactor.get(name="Illumina NovaSeq 6000")
+efo_10x = bt.ExperimentalFactor.get(name="10X 3' v1")
+exp001 = ln.ULabel.get(name="EXP002")
+s001 = ln.ULabel.get(name="S001")
 
 # register output files of the sequencer
-ln.Artifact("s3://lamindata/fastq/schmidt22_perturbseq_R1_001.fastq.gz").save()
-ln.Artifact("s3://lamindata/fastq/schmidt22_perturbseq_R2_001.fastq.gz").save()
+raw_files = [
+    "s3://lamindata/fastq/schmidt22_perturbseq_R1_001.fastq.gz",
+    "s3://lamindata/fastq/schmidt22_perturbseq_R2_001.fastq.gz",
+]
+for raw_file in raw_files:
+    artifact = ln.Artifact(raw_file).save()
+    artifact.labels.add(novaseq6000, feature=features.instrument)
+    artifact.labels.add(efo_10x, feature=features.technology)
+    artifact.labels.add(exp001, feature=features.experiment)
+    artifact.labels.add(s001, feature=features.biosample)
 
 ln.finish()
