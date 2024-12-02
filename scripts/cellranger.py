dde1f662470bd8169ff57324eb02934726c461d4
Sunny Sun <38218185+sunnyosun@users.noreply.github.com>
2024-12-02 22:09:11 +0100

    Refactor notebooks

--- scripts/cellranger.py
+++ scripts/cellranger.py
@@ -1,13 +1,16 @@
 import lamindb as ln
 
 # register the pipeline and track input/output artifacts
-transform = ln.Transform(
-    name="Cell Ranger",
-    version="7.2.0",
-    type="pipeline",
-    reference="https://www.10xgenomics.com/support/software/cell-ranger/7.2",
-)
-ln.track(transform=transform)
+transform = ln.Transform.get(name="Cell Ranger", version="9.0.0")
+if transform is None:
+    transform = ln.Transform(
+        name="Cell Ranger",
+        version="9.0.0",
+        type="pipeline",
+        reference="https://www.10xgenomics.com/support/software/cell-ranger/9.0",
+    ).save()
+
+ln.track(transform=transform, params={"project": "schmidt22"})
 # access uploaded files as inputs for the pipeline
 input_artifacts = ln.Artifact.filter(key__startswith="fastq/schmidt22_perturbseq").all()
 input_paths = [artifact.cache() for artifact in input_artifacts]
@@ -16,3 +19,7 @@ output_artifacts = ln.Artifact.from_dir(
     "s3://lamindata/schmidt22_perturbseq/filtered_feature_bc_matrix"
 )
 ln.save(output_artifacts)
+
+for artifact in output_artifacts:
+    artifact.labels.add_from(input_artifacts[0])
+    artifact.features._add_from(input_artifacts[0])
--- scripts/postprocess_cellranger.py
+++ scripts/postprocess_cellranger.py
@@ -1,15 +1,12 @@
-import bionty as bt
 import lamindb as ln
 
 # Post-process 3 cellranger output files
-ln.settings.transform.stem_uid = "piG5scNASXcc"
-ln.settings.transform.version = "1"
 ln.settings.sync_git_repo = "https://github.com/laminlabs/rnd-demo"
-run = ln.track(params={"project": "schmidt22"})
+ln.track("piG5scNASXcc5zKv", params={"project": "schmidt22"})
 # label the transform
-ulabel1 = ln.ULabel(name="use-case").save()
-ulabel2 = ln.ULabel(name="schmidt22").save()
-run.transform.ulabels.add(ulabel1, ulabel2)
+ulabel1 = ln.ULabel.get(name="use-case")
+ulabel2 = ln.ULabel.get(name="schmidt22")
+ln.context.transform.ulabels.add(ulabel1, ulabel2)
 
 # Download the input artifacts
 input_artifacts = ln.Artifact.filter(
@@ -20,20 +17,12 @@ input_artifacts = ln.Artifact.filter(
 # Register the output artifact
 output_artifact = ln.Artifact(
     "s3://lamindata/schmidt22_perturbseq/schmidt22_perturbseq.h5ad",
-    description="perturbseq counts",
+    description="schmidt22 perturbseq counts",
 )
 output_artifact.save()
 
 # Add labels to the output artifact
-efo = bt.ExperimentalFactor.lookup()
-features = ln.Feature.lookup()
-output_artifact.labels.add(efo.perturb_seq, features.assay)
-output_artifact.labels.add(efo.single_cell_rna_sequencing, features.readout)
-is_experiment = ln.ULabel.filter(name="is_experiment").one()
-is_biosample = ln.ULabel.filter(name="is_biosample").one()
-exp2 = is_experiment.children.filter(description__contains="Perturb-seq").one()
-biosample = is_biosample.children.get(name="S001")
-output_artifact.labels.add(exp2, features.experiment)
-output_artifact.labels.add(biosample, features.biosample)
+output_artifact.labels.add_from(input_artifacts[0])
+output_artifact.features._add_from(input_artifacts[0])
 
 ln.finish()
