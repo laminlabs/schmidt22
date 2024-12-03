db71c6a7e975cbea751822f656d27a7b8845bc8c
Sunny Sun <38218185+sunnyosun@users.noreply.github.com>
2024-12-03 19:20:23 +0100

    🎨 Update transform version

--- scripts/chromium_10x_upload.py
+++ scripts/chromium_10x_upload.py
@@ -4,7 +4,7 @@ import wetlab as wl
 
 # track the current python script
 ln.settings.sync_git_repo = "https://github.com/laminlabs/rnd-demo"
-ln.track("J5ZTmVxSch3U0002", params={"project": "schmidt22"})
+ln.track("J5ZTmVxSch3U0003", params={"project": "schmidt22"})
 # label the transform
 ulabel1 = ln.ULabel.get(name="use-case")
 ulabel2 = ln.ULabel.get(name="schmidt22")
