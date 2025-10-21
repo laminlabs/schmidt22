75bed951cd0a69e84ad5e086ff048c50337a814b
Alex Wolf <f.alexander.wolf@gmail.com>
2025-10-21 13:38:53 +0200

    ✨ Add define_entities.py script

---
+++ scripts/define_entities.py
@@ -0,0 +1,32 @@
+import lamindb as ln
+import bionty as bt
+
+
+def main():
+    ln.track(project="Schmidt22")
+
+    biosample_type = ln.Record(
+        name="Biosample", description="A biological sample", is_type=True
+    ).save()
+    experiment_type = ln.Record(
+        name="Experiment", description="An experiment", is_type=True
+    ).save()
+
+    ln.Feature(
+        name="biosample", dtype=biosample_type, description="Biosample record"
+    ).save()
+    ln.Feature(
+        name="experiment", dtype=experiment_type, description="Experiment record"
+    ).save()
+    ln.Feature(
+        name="assays_efo",
+        dtype=list[bt.ExperimentalFactor],
+        description="EFO term describing an assay",
+    ).save()
+    ln.Feature(
+        name="readouts_efo",
+        dtype=list[bt.ExperimentalFactor],
+        description="EFO term describing a readout",
+    ).save()
+
+    ln.finish()
