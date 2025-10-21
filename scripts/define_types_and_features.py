6f8cadfb1360ac16ccb6d5e43f213b4d5b1b9fc2
Alex Wolf <f.alexander.wolf@gmail.com>
2025-10-21 14:41:18 +0200

    ✨ Add `define_types_and_features.py` (#8)
    * ✨ Add define_entities.py script
    
    * ♻️ Refactor
    
    * ♻️ Rename
    
    * ♻️ Rename
    
    * ♻️ Simplify CI
    
    * ♻️ Delete other scripts
    
    * 💚 Fix
    
    * 💚 Fix

---
+++ scripts/define_types_and_features.py
@@ -0,0 +1,23 @@
+import lamindb as ln
+import bionty as bt
+
+
+def main():
+    # define project only a single time
+    ln.Project(name="Schmidt22").save()
+    # track this script
+    ln.track(project="Schmidt22")
+    # define types
+    biosample_type = ln.Record(name="Biosample", is_type=True).save()
+    experiment_type = ln.Record(name="Experiment", is_type=True).save()
+    # define features
+    ln.Feature(name="biosample", dtype=biosample_type).save()
+    ln.Feature(name="experiment", dtype=experiment_type).save()
+    ln.Feature(name="assays_efo", dtype=list[bt.ExperimentalFactor]).save()
+    ln.Feature(name="readouts_efo", dtype=list[bt.ExperimentalFactor]).save()
+    # finish this script
+    ln.finish()
+
+
+if __name__ == "__main__":
+    main()
