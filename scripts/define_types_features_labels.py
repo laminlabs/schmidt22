6e694503e1f93e8fca0056a1c9789e096cf2f790
Alex Wolf <f.alexander.wolf@gmail.com>
2025-10-22 17:01:58 +0200

    ✨ Add notebooks related to curating and analyzing phenotypic data (#14)
    * ♻️ Extend labels
    
    * ♻️ Allow notebooks
    
    * ♻️ Add notebooks
    
    * ♻️ Install ipykernel
    
    * 💚 Fix
    
    * 💚 Fix
    
    * 💚 Fix
    
    * 💚 Fix
    
    * ♻️ Prettify
    
    * ♻️ Debug
    
    * 💚 Attempt fix
    
    * 💚 Fix
    
    * ♻️ Back to main branch

--- scripts/define_types_features_labels.py
+++ scripts/define_types_features_labels.py
@@ -19,14 +19,28 @@ def main():
     ln.Feature(name="library_preparation", dtype=bt.ExperimentalFactor).save()
     ln.Feature(name="technology", dtype=bt.ExperimentalFactor).save()
     ln.Feature(name="instrument", dtype=bt.ExperimentalFactor).save()
+    ln.Feature(name="original_publication", dtype=ln.Reference.abbr).save()
     # create labels for samples and experiments
     ln.Record(name="Schmidt22-S001", type=biosample_type).save()
     ln.Record(name="Schmidt22-EXP001", type=experiment_type).save()
     ln.Record(name="Schmidt22-EXP002", type=experiment_type).save()
     # create labels for instrument, technology, library preparation
-    bt.ExperimentalFactor.from_source(name="Illumina NovaSeq 6000").save()
-    bt.ExperimentalFactor.from_source(name="Perturb-Seq").save()
-    bt.ExperimentalFactor.from_source(name="10x 3' v2").save()
+    bt.ExperimentalFactor.from_values(
+        [
+            "Illumina NovaSeq 6000",
+            "Perturb-Seq",
+            "gRNA-seq",
+            "10x 3' v2",
+            "interferon gamma",
+        ]
+    ).save()
+    # create the reference
+    ln.Reference(
+        abbr="Schmidt22",
+        name="CRISPR activation and interference screens decode stimulation responses in primary human T cells",
+        pubmed_id=35113687,
+        text="Regulation of cytokine production in stimulated T cells can be disrupted in autoimmunity, immunodeficiencies, and cancer. Systematic discovery of stimulation-dependent cytokine regulators requires both loss-of-function and gain-of-function studies, which have been challenging in primary human cells. We now report genome-wide CRISPR activation (CRISPRa) and interference (CRISPRi) screens in primary human T cells to identify gene networks controlling interleukin-2 (IL-2) and interferon-γ (IFN-γ) production. Arrayed CRISPRa confirmed key hits and enabled multiplexed secretome characterization, revealing reshaped cytokine responses. Coupling CRISPRa screening with single-cell RNA sequencing enabled deep molecular characterization of screen hits, revealing how perturbations tuned T cell activation and promoted cell states characterized by distinct cytokine expression profiles. These screens reveal genes that reprogram critical immune cell functions, which could inform the design of immunotherapies.",
+    ).save()
     # create features for cell ranger run
     ln.Feature(name="transcriptome_link", dtype=str).save()
     # finish the script
